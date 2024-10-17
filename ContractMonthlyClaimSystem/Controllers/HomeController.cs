2bff157a6bb95aef3e9563534881781f54de2baa
lemi-manqoyi <liyemamanqoyi23+GITHUB@gmail.com>
2024-10-17 21:36:30 +0200

    so much changed added new views

--- ContractMonthlyClaimSystem/Controllers/HomeController.cs
+++ ContractMonthlyClaimSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using ContractMonthlyClaimSystem.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,23 +8,70 @@ namespace ContractMonthlyClaimSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+       
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
+
+        
+
+        //private list to store all the user uploads
+        private static List<LecturerModel> claims = new List<LecturerModel>();
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+         //http post, to store the file in a folder I created in the webroot
+        [HttpPost]
+        public IActionResult Upload(IFormFile file, string uploaderName, string addNotes, int hoursWorked, double hourlyRate)
+        {
+            if (file != null && file.Length > 0)
+            {
+               var fileName = Path.GetFileName(file.FileName); 
+               var path = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                claims.Add(new LecturerModel
+                {
+                    LecturerID = claims.Count +1,
+                    LecturerUploadFileName = fileName,
+                    LecturerAdditionalNotes = addNotes,
+                    LecturerName = uploaderName,
+                    LecturerHourlyRate = hourlyRate,
+                    LecturerHoursWorked = hoursWorked,
+                    LecturerUploadDate = DateTime.Now,
+                });
+
+            }
+            return RedirectToAction("LecturerSubmitClaim");
+        }
+
+        //method to open the file
+        public ActionResult OpenFile(string fileName)
+        {
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads", fileName);
+            var fileBytes = System.IO.File.ReadAllBytes(path);
+            return File(fileBytes, "application/octet-stream", fileName);
+        }
+        public IActionResult LecturerSubmitClaim()
+        {
+            return View(claims);
+        }
+        
+        public IActionResult LecturerMenu()
         {
             return View();
         }
-
-        public IActionResult LecturerSubmitClaim()
+        
+        public IActionResult AdminMenu()
         {
             return View();
         }
--- ContractMonthlyClaimSystem/Models/LecturerModel.cs
+++ ContractMonthlyClaimSystem/Models/LecturerModel.cs
@@ -3,8 +3,19 @@
     public class LecturerModel
     {
 
+        public int LecturerID { get; set; }
 
+        public string LecturerName { get; set; }  required
+        public string LecturerUploadFileName { get; set; }  required
 
+        public string LecturerAdditionalNotes { get; set; }  required
+
+        public double LecturerHourlyRate{ get; set; } required
+
+        public int LecturerHoursWorked{ get; set; } 
+
+        public DateTime LecturerUploadDate { get; set; }
 
     }
+
 }
